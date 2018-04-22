e599d2f2f6d907baa557b4e7218299f143a336ce
Elizabeth Richardson <emrichardson820@gmail.com>
2018-04-22 12:12:28 -0700

    more code, reogranized functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,71 +14,67 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
+function promptOrder() {
+  inquirer
+    .prompt([
+      {
+        name: "productId",
+        message: "What is the ID of the product you would like to buy?"
+      },
+      {
+        name: "quantity",
+        message: "How many units would you like to buy?"
+      }
+    ])
+    .then(function(answer) {
+      const item = answer.productId;
+      const quantity = answer.quantity;
+
+      console.log(
+        "\n You have selected " +
+          quantity +
+          " units of item ID: " +
+          item +
+          ". \n"
+      );
+
+      connection.query("SELECT * FROM products", { item_id: item }, function(
+        err,
+        res
+      ) {
+        if (err) throw err;
+        if (data.length === 0) {
+          console.log(
+            "Error: Invalid item ID. Please re-order a valid item ID."
+          );
+          // availableProducts();
+        } else {
+          if (quantity > data[0].stock_quantity) {
+            console.log("Your order has been processed and is confirmed.");
+          } else {
+            console.log(
+              "Unfortunately the ordered quantity is not in stock, please reorder at a different quantity.\n"
+            );
+            // availableProducts();
+          }
+        }
+        connection.end();
+      });
+    });
+}
+
 function availableProducts() {
   console.log("\n Here are the items available for purchase: \n");
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     // Loop all results
     console.table(res);
-    connection.end();
+    promptOrder();
   });
 }
 
-// availableProducts();
-
-function Order(name, productId, quantity) {
-  this.name = name;
-  this.productId = productId;
-  this.quantity = quantity;
-  //creates printInfo method and applies to all Order objects
-  this.printInfo = function() {
-    console.log(
-      "Customer, " +
-        this.name +
-        ", has placed an order for " +
-        this.quantity +
-        " units of " +
-        "Product ID# " +
-        this.productId
-    );
-  };
+function runOrder() {
+  availableProducts();
 }
 
-var answerArr = [];
-
-//runs inquirer and asks user their name for the order, the product they wish to order and the quantity
-inquirer
-  .prompt([
-    {
-      name: "name",
-      message: "What is your name?"
-    },
-    {
-      name: "productId",
-      message: "What is the ID of the product you would like to buy?"
-    },
-    {
-      name: "quantity",
-      message: "How many units would you like to buy?"
-    }
-  ])
-  .then(function(answers) {
-    var newOrder = new Order(answers.name, answers.productId, answers.quantity);
-    console.log(newOrder);
-    answerArr.push(newOrder);
-    console.log(answerArr);
-    newOrder.printInfo();
-  });
-
-//push answers into an array!
-//pull print info out of the constructor and make it it's own function so it doesn't print in the pushed array??
-
-// //!! instead of answers and this, use array that answers are pushed to?
-// function checkInventory() {
-//     if (answers.quantity > this.inventory) {
-//         console.log("Insufficient quantity!");
-//     }
-//     else {
-//         console.log("Order processing")
-//     }
-// };
+runOrder();
