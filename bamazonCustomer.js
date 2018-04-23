1d7f0d0143edd5052dc21f80795b81ede108573e
Elizabeth Richardson <emrichardson820@gmail.com>
2018-04-22 17:04:35 -0700

    cleaned up functions, still working on order confirmation functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,16 +14,19 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
+
 function promptOrder() {
   inquirer
     .prompt([
       {
         name: "productId",
-        message: "What is the ID of the product you would like to buy?"
+        message: "What is the ID of the product you would like to buy?",
+        filter: Number,
       },
       {
         name: "quantity",
-        message: "How many units would you like to buy?"
+        message: "How many units would you like to buy?",
+        filter: Number,
       }
     ])
     .then(function(answer) {
@@ -43,19 +46,17 @@ function promptOrder() {
         res
       ) {
         if (err) throw err;
-        if (data.length === 0) {
+        if (res.length === 0) {
           console.log(
             "Error: Invalid item ID. Please re-order a valid item ID."
           );
-          // availableProducts();
         } else {
-          if (quantity > data[0].stock_quantity) {
+          if (quantity > res[0].stock_quantity) {
             console.log("Your order has been processed and is confirmed.");
           } else {
             console.log(
-              "Unfortunately the ordered quantity is not in stock, please reorder at a different quantity.\n"
+              "Unfortunately the ordered quantity is not in stock, please re-order at a different quantity.\n"
             );
-            // availableProducts();
           }
         }
         connection.end();
@@ -63,18 +64,17 @@ function promptOrder() {
     });
 }
 
-function availableProducts() {
-  console.log("\n Here are the items available for purchase: \n");
-  connection.query("SELECT * FROM products", function(err, res) {
+function showInventory() {
+  connection.query('SELECT * FROM products', function (err, res){
     if (err) throw err;
-    // Loop all results
     console.table(res);
     promptOrder();
-  });
+  })
 }
 
+
 function runOrder() {
-  availableProducts();
+  showInventory();
 }
 
 runOrder();
